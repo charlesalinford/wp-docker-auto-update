def0ff87e19b26a6b28501fc659c605910b92ff4
Charles Linford <charles@vi.com.au>
2017-11-16 16:10:37 +1100

    Updated setup to automatically look for setup credentials

--- README.md
+++ README.md
@@ -4,17 +4,18 @@ VI node.js script for auto-updating WordPress core and packages on Docker projec
 ## Installation
 1. Clone repository into /docker folder e.g. /medview.com.au/devops/docker
 
-2. cd into the repository folder, then run 'npm install'
+2. cd into the repository folder, then run 'npm install', and follow the prompts to add credentials to the generated .env file
 
-3. run 'docker ps', then copy the ID *or* Name of the PHP container, e.g. d8a4aaeac3f0 (ID) *or* php--medview-com-au (Name)
+3. run 'node index' to start the script
 
-3a. open the .env file in the /docker folder, and take note of the DOMAIN value e.g. DOMAIN=medview.com.au
+## Adding credentials to .env file (optional)
+1. run 'docker ps', then copy the ID *or* Name of the PHP container, e.g. d8a4aaeac3f0 (ID) *or* php--medview-com-au (Name)
 
-3. Open the .env file and add the following details:
+2. open the .env file in the parent /docker folder, and take note of the DOMAIN value e.g. DOMAIN=medview.com.au
+
+3. Open the generated .env file (after running 'npm install') and add the following details:
 	- PHP_CONTAINER=[ name/ID of the PHP container ]
 	- DOMAIN=[ DOMAIN .env value ]
-
-4. run 'node index' to start the script
 
 ## Troubleshooting
 * Message: */bin/bash: wp: command not found*
--- setup.js
+++ setup.js
@@ -1,11 +1,63 @@
 'use strict';
 
 var fs = require('fs'),
-colors = require('colors');
+colors = require('colors'),
+execs = require('child_process').execSync,
+domain = 'DOMAIN=',
+containers = '';
 
-fs.writeFile(".env", "#Docker credentials\nDOMAIN=\nPHP_CONTAINER=", function(err) {
-    if(err) {
-        return console.log(err);
-    }
-    console.log(".env file created.".yellow);
-});
+setup();
+
+function setup(){
+
+	//Look for .env file in parent directory
+	try {
+		var parent_env = fs.readFileSync('../.env', "UTF-8");
+		var lines = parent_env.split('\n');
+	    for(var line = 0; line < lines.length; line++){
+	        if(lines[line].indexOf('DOMAIN=') !== -1){
+	            domain = lines[line];
+			}
+	    }
+	    console.log('Domain found in parent .env file: ' + domain + '\n');
+
+	} catch (err) {
+		console.log('Error encountered, no DOMAIN var added to .env file. \n');
+	}
+
+	//Get name of container that contains "php" in the name
+	containers = execs('docker ps --filter "name=php" --format "{{.Names}}"');
+
+	//If no container found, ask user to fill .env file manually
+	if(containers == ''){
+		console.log('No PHP container found. Please enter project credentials into the .env file.');
+		createEnv(domain);
+	}
+	else{
+		console.log('PHP container found: ' + containers);
+		console.log('Is this the correct PHP container for this project? ');
+
+		//Use user input to add/reject current Docker container
+		process.stdin.on('data', function(data){
+			var answer = data.toString().trim();
+			if(answer == 'y' || answer == 'yes'){
+				console.log(containers.toString().trim() + ' will be added to .env file.');
+				createEnv(domain, containers.toString().trim());
+			}
+			else{
+				console.log(containers.toString().trim() + ' has not been added to .env file. Please enter project credentials manually.');
+				createEnv(domain);
+			}
+		});
+	}
+}
+
+function createEnv(domain, container_name = ''){
+	fs.writeFileSync(".env", "#Docker credentials\n" + domain + "\nPHP_CONTAINER=" + container_name,function(err) {
+	    if(err) {
+	        return console.log(err);
+	    }
+	    console.log(".env file created.");
+    });
+    process.exit();
+}
