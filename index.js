f7e5232d338e41850b554412156bfd38fce9d20e
Charles Linford <charles@vi.com.au>
2017-11-27 11:10:04 +1100

    Changed path to absolute for update commands

--- index.js
+++ index.js
@@ -10,10 +10,10 @@ console.log('Running update script'.yellow);
 
 //Check for core updates
 console.log('Step 1: Checking for core updates...'.yellow);
-execs('docker exec -t -i ' + container_id + ' /bin/bash -c "cd ../vhosts/' + site_id + '/public && wp core update --allow-root"', { stdio:'inherit' });
+execs('docker exec -t -i ' + container_id + ' /bin/bash -c "cd /var/www/vhosts/' + site_id + '/public && wp core update --allow-root"', { stdio:'inherit' });
 
 //Check for plugin updates
 console.log('Step 2: Checking for plugin updates...'.yellow);
-execs('docker exec -t -i ' + container_id + ' /bin/bash -c "cd ../vhosts/' + site_id + '/public && wp plugin update --all --allow-root"', { stdio:'inherit' });
+execs('docker exec -t -i ' + container_id + ' /bin/bash -c "cd /var/www/vhosts/' + site_id + '/public && wp plugin update --all --allow-root"', { stdio:'inherit' });
 
 console.log('Updates have been completed.'.yellow);
